312c2bd8496defb3ea9b29ca83fbf945c7a1ab4f
uslesssss <826120585@qq.com>
2022-09-05 10:53:22 +0800

    用户自定义结构递归

--- demo/test.py
+++ demo/test.py
@@ -4,37 +4,11 @@ import sys
 import base64
 
 
-def final_deal(s):  # s为json字符串
-    lsts = json.loads(s)
-    print(lsts)
-    dic = {}
-    for i, j in zip(lsts[0].values(), lsts[0].keys()):
-        if not i:
-            continue
-        dic[j] = demo.recog(str(i))
 
-    lsts_ = []
 
-    for lst in lsts:
-        dic_temp = {}
-        lsts_.append(dic_temp)
-        for i in lst.keys():
-            cla = dic.get(i)
 
-            if not cla:
-                continue
-            else:
-                if cla[1] == 'AllType':
-                    dic_temp[f'{i}_cover'] = cla[0](str(lst[i])).cover()
-                else:
-                    dic_temp[f'{i}_cover'] = cla[0](str(lst[i])).cover()
-                    dic_temp[f'{i}_shuffle'] = cla[0](str(lst[i])).shuffle()
-                    dic_temp[f'{i}_md'] = cla[0](str(lst[i])).md()
-                    dic_temp[f'{i}_randmapping'] = cla[0](str(lst[i])).randmapping()
 
-    for i, j in zip(lsts, lsts_):
-        i.update(j)
-    return lsts
+
 
 
 if __name__ == '__main__':
